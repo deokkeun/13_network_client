4220835d161ce3c00cf72814313263bb4d3c65e5
DeokKeun <deokk91@gmail.com>
2023-01-03 13:52:32 +0900

    13_network_client

---
+++ src/client1/edu/kh/network/run/ClientRun1.java
@@ -0,0 +1,13 @@
+package client1.edu.kh.network.run;
+
+import client1.edu.kh.network.model.service.TCPClient1;
+
+public class ClientRun1 {
+
+	public static void main(String[] args) {
+
+		TCPClient1 client = new TCPClient1();
+		client.clientSart();
+	}
+
+}
